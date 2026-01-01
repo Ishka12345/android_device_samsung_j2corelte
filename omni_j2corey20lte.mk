769c2f0ac4285b6a389747c0d29d2f4b7cd94893
Android BuildBot <android@build.bot>
2026-01-01 09:17:00 -0500

    Fix: Inherit from common TWRP config

--- omni_j2corey20lte.mk
+++ omni_j2corey20lte.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from j2corey20lte device
 $(call inherit-product, device/samsung/j2corey20lte/device.mk)
